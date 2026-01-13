8f987275c37ce4e491f3553a9ac456ce640dafcf
gmrp22 <gmrp22@hotmail.com>
2026-01-13 11:57:04 -0600

    first rendering problem

---
+++ rendering-problems/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ProblematicParent from './first-problem/index.jsx'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <ProblematicParent />
+    </>
+  )
+}
+
+export default App
